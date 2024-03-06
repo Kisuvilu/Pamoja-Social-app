698e503a763cf8ae200a0a1e616f5396937a12ee
Dennis Musembi @kisuvilu <71897350+Kisuvilu@users.noreply.github.com>
2024-03-06 05:11:52 +0300

    My login page

---
+++ login.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pamoja App</title>
+</head>
+<style >
+    #blue bar{
+        height:100px;
+        background-color: rgb(59,89,152); 
+        color: #d9dfeb; 
+        font-size: 40px;">
+        padding: 4px;
+    }
+
+    #Signup_button{
+        background-color: #42b72a;
+        width:70px;
+        text-align: center
+        padding: 4px;
+        border-radius: 4px;
+        float: right
+    }
+
+    #login_bar{
+        background-color: white; 
+        width: 800px; 
+        height: 200px; 
+        margin:auto;
+        margin top: 50px"
+        padding-top: 50px;
+        text-align: center;
+        font-weight: bold;
+
+    }
+
+    #text{
+        height:300px;
+        width:400px;
+        border-radius: 4px;
+        border: solid 1px #aaa;
+        padding: 4px;
+        font-size: 14;
+
+    }
+
+    #button{
+        width: 300px;
+        height: 40px;
+        border-radius: 4px;
+        font-weight: bold;
+        border: none;
+        background-color: rgb(59,89,152); 
+        color: white;
+        font-weight: bold;
+
+
+    }
+</style>
+<body style="font-family: tahoma; background-color: #e9ebee;">
+    <div id="blue bar">
+     <div style="font-size: 40px;">Pamoja App</div>
+     <div id= "Signup_button">Signup</div>
+
+    </div>
+
+    <div id="login_bar">
+        Log in to Pamoja App
+        <input type="text" id="text" placeholder="Email or Phone number><br><br>
+        <input type="password" id="text" placeholder="Password>><br><br>
+        <input type="submit" id="button" value="Log in">
+        <br><br><br>
+    </div>
+
+</body>
+</html>
